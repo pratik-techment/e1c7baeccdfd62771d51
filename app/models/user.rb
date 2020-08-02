5c537808e2444c13b0dbea99302a5abecfd68eb8
Pratik Raut <pratik.raut@techment.com>
2020-08-02 04:24:27 -0700

    added relationship table

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,35 @@ class User < ApplicationRecord
 
    has_many :tweets, dependent: :destroy
 
+   has_many :active_relationships, class_name:  "Relationship",
+                                  foreign_key: "follower_id",
+                                  dependent:   :destroy
+
+   has_many :passive_relationships, class_name:  "Relationship",
+                                     foreign_key: "followed_id",
+                                     dependent:   :destroy
+
+
+    has_many :following, through: :active_relationships, source: :followed
+    has_many :followers, through: :passive_relationships, source: :follower
+
    def feed
      #tweets from all users followed by current user
    end
+
+
+  # Follows a user.
+  def follow(other_user)
+    active_relationships.create(followed_id: other_user.id)
+  end
+
+  # Unfollows a user.
+  def unfollow(other_user)
+    active_relationships.find_by(followed_id: other_user.id).destroy
+  end
+
+  # Returns true if the current user is following the other user.
+  def following?(other_user)
+    following.include?(other_user)
+  end
 end
